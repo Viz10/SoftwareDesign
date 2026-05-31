b9fe47246e3d38e1d9722e30b9612ffe6db32f5a
unknown <raulvirag@yahoo.com>
2026-06-01 01:15:46 +0300

    Chat ok

---
+++ Warehouse/NotificationService/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+ 
+namespace NotificationService.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user,string message)
+        {
+            ///calls the "ReceiveMessage" function on all connected clients
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"[CHAT] Connected: {Context.ConnectionId}");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            Console.WriteLine($"[CHAT] Disconnected: {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- Warehouse/NotificationService/Program.cs
+++ Warehouse/NotificationService/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using NotificationService.Hubs;
 using Scalar.AspNetCore;
 using System.Reflection;
 using Warehouse.Shared.Auth;
@@ -18,12 +19,24 @@ builder.Services.AddTransient<AuthTokenHandler>();
 builder.Services.AddHttpClient("AccountService", c => c.BaseAddress = new Uri(builder.Configuration["AccountServiceURL"]!))
     .AddHttpMessageHandler<AuthTokenHandler>();
 
+builder.Services.AddSignalR();
 builder.Services.AddControllers(options =>
 {
     var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
     options.Filters.Add(new AuthorizeFilter(policy));
 });
 builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor", policy =>
+    {
+        policy.WithOrigins(builder.Configuration["BlazorURL"]!)
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -36,10 +49,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowBlazor");
+
 app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<ChatHub>("/hubs/chat");
 
 app.Run();
